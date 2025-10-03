bb9137713de6fee861e75f78bd880d3174023f3b
wanqian-windrunner <2287317341@qq.com>
2025-10-03 19:03:46 +0800

    add loading & progress-bar

--- assets/script.js
+++ assets/script.js
@@ -18,4 +18,10 @@
       }, 500);
     }, remaining);
   });
-
+// 进度条
+window.onscroll = function() {
+    var docHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    var progress = (scrollTop / docHeight) * 100.0;//控制进度条长度
+    document.getElementById('progress-bar').style.width = progress + '%';
+};
